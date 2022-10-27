8bf7345e3a11e6e9eef36c2d2369f935c5150615
Fatima Musa <mpatigul@my.centennialcollege.ca>
2022-10-26 21:23:23 -0400

    Books List Added

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,3 @@
-export const MongoURI = "mongodb://localhost:27017/media";
+//export const MongoURI = "mongodb://localhost:27017/media";
+export const MongoURI = "mongodb+srv://FatimaMusa:Fatima7425f@cluster0.atk0exu.mongodb.net/media?retryWrites=true&w=majority";
 export const Secret = "someSecret";
